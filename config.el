4e749b83cbb774f015648546554bcfb1d4c51769
Volodymyr Yavdoshenko <v.yavdoshenko@gmail.com>
2022-05-27 08:39:48 +0300

    rust/python env updated

--- config.el
+++ config.el
@@ -28,18 +28,20 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
-(setq doom-font (font-spec :family "Fira Code" :size 30 :weight 'light))
-(setq doom-variable-pitch-font (font-spec :family "Noto Sans" :size 24 :weight 'extra-light))
+(setq doom-font (font-spec :family "Fira Code" :size 28 :weight 'light))
+(setq doom-variable-pitch-font (font-spec :family "Noto Sans" :size 26 :weight 'extra-light))
 
 (when (eq system-type 'darwin)
-  (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 17 :weight 'light))
+  (setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'extra-light))
   (setq doom-variable-pitch-font (font-spec :family "Noto Sans" :size 13 :weight 'extra-light)))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-monokai-classic)
-(setq doom-themes-treemacs-theme "doom-colors")
+;(setq doom-theme 'doom-monokai-classic)
+;(setq doom-themes-treemacs-theme "doom-colors")
+(add-to-list 'custom-theme-load-path "~/doomthemes")
+(load-theme 'mariana t)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
