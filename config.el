8f27274788347e4c527c8e5f59d76a38a9480746
Volodymyr Yavdoshenko <v.yavdoshenko@gmail.com>
2022-07-02 14:57:39 +0300

    configs updated

--- config.el
+++ config.el
@@ -38,10 +38,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-;(setq doom-theme 'doom-monokai-classic)
-;(setq doom-themes-treemacs-theme "doom-colors")
-(add-to-list 'custom-theme-load-path "~/doomthemes")
-(load-theme 'mariana t)
+(setq doom-theme 'doom-one)
+(setq doom-themes-treemacs-theme "doom-colors")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
